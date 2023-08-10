ec3e730723d525c37b3d5b34e84bc0403e7b6898
Pavel-Fomchenkov <donotwritemail@gmail.com>
2023-08-11 01:24:16 +0300

    hw5 corrected

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
     public static void task3() {
         short year = 2100;
-        if ((year % 2 == 0 && year % 100 != 0) || year % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else System.out.println(year + " год не является високосным");
     }
